4d9fca5ba76d8a7f5b3305ea5989f73ead189adf
RadaSangOn <radasangon@gmail.com>
2016-05-24 16:08:22 +0700

    65675

--- test1/src/test1/Controllers/HomeController.cs
+++ test1/src/test1/Controllers/HomeController.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using RazorPDF;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
 
 namespace test1.Controllers
 {
@@ -33,10 +36,48 @@ namespace test1.Controllers
             return View();
         }
 
-        public PdfResult Pdf()
+        public IActionResult test1()
         {
-            // With no Model and default view name.  Pdf is always the default view name
-            return new PdfResult();
+            MemoryStream workStream = new MemoryStream();
+            Document document = new Document();
+            var writer = PdfWriter.GetInstance(document, workStream);
+
+            document.Open();
+            //document.Add(new Paragraph("Hello World"));
+            //document.Add(new Paragraph(DateTime.Now.ToString()));
+
+            using (var html = new StringReader(View("Contact").ToString()))
+            {
+                XMLWorkerHelper.GetInstance().ParseXHtml(writer, document, html);
+                
+            }
+
+            document.Close();
+
+            byte[] byteInfo = workStream.ToArray();
+            workStream.Write(byteInfo, 0, byteInfo.Length);
+            workStream.Position = 0;
+
+            return new FileStreamResult(workStream, "application/pdf");
+        }
+
+        // GET: members/DetailsPdf/5
+        public FileStreamResult DetailsPdf(String id)
+        {
+            MemoryStream workStream = new MemoryStream();
+            Document document = new Document();
+            PdfWriter.GetInstance(document, workStream).CloseStream = false;
+
+            document.Open();
+            document.Add(new Paragraph("Hello World"));
+            document.Add(new Paragraph(DateTime.Now.ToString()));
+            document.Close();
+
+            byte[] byteInfo = workStream.ToArray();
+            workStream.Write(byteInfo, 0, byteInfo.Length);
+            workStream.Position = 0;
+
+            return new FileStreamResult(workStream, "application/pdf");
         }
     }
 }
