d4776e3a12aff9130184a0db80daa62773e487be
radasangon <radasangon@gmail.com>
2016-05-26 22:24:48 +0700

    retrt

---
+++ PPcore/src/PPcore/Models/PalangPanyaDBContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PalangPanya.Models
+{
+    public partial class PalangPanyaDBContext : DbContext
+    {
+        public PalangPanyaDBContext(DbContextOptions<PalangPanyaDBContext> options)
+            : base(options)
+        { }
+
+        public virtual DbSet<member> member { get; set; }
+    }
+}
--- PPcore/src/PPcore/Startup.cs
+++ PPcore/src/PPcore/Startup.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using PalangPanya.Models;
 
 namespace PPcore
 {
@@ -27,6 +25,9 @@ namespace PPcore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var connection = @"Server=(localdb)\ProjectsV13;Initial Catalog=PPCoreDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<PalangPanyaDBContext>(options => options.UseSqlServer(connection));
+
             // Add framework services.
             services.AddMvc();
         }
