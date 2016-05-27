54157fec841d927086eb720cb0b8aaebbfad00c5
radasangon <radasangon@gmail.com>
2016-05-27 08:29:59 +0700

    trhtrfyrty

--- PPcore/src/PPcore/wwwroot/_references.js
+++ PPcore/src/PPcore/wwwroot/_references.js
@@ -10,6 +10,7 @@
 /// <reference path="lib/holderjs/holder.js" />
 /// <reference path="lib/html5shiv/dist/html5shiv.js" />
 /// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-ui/jquery-ui.js" />
 /// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
 /// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="lib/metismenu/dist/metisMenu.js" />
